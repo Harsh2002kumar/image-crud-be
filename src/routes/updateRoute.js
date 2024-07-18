edaac1bade04dc3dfb5f72ec174274309d497f1f
Harsh2002kumar <harshkumarpandey2002@gmail.com>
2024-07-18 09:17:31 +0530

    connected mongodb using mongoose

--- src/routes/updateRoute.js
+++ src/routes/updateRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const updatePicture = require("../controllers/updateController").default;
+const updatePicture = require("../controllers/updateController");
 
 router.put("/", updatePicture);
 
--- src/server.js
+++ src/server.js
@@ -5,9 +5,24 @@ const uploadRoute = require("./routes/uploadRoute");
 const updateRoute = require("./routes/updateRoute");
 const deleteRoute = require("./routes/deleteRoute");
 const fetchRoute = require("./routes/fetchRoute");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 const PORT = 9000;
+
+// Connect to mongodb
+mongoose
+  .connect(
+    "mongodb+srv://hkumar05022002:harsh2004@cluster0.f4snkvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error("Error in connecting to MongoDB", err));
 
 //Middlewares
 app.use(cors());
@@ -20,3 +35,7 @@ app.use("/upload", uploadRoute);
 app.use("/update", updateRoute);
 app.use("/delete", deleteRoute);
 app.use("/fetch", fetchRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
